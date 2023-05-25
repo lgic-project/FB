54d4992bbed32a301b9f6c475efb8e1ac49186d4
Nabin788 <123879198+Nabin788@users.noreply.github.com>
2023-05-25 21:55:14 +0545

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import{ Link} from 'react-router-dom';
+
+import{ Link, useNavigate} from 'react-router-dom';
 const Nav=()=>{
+    // after signup it will hide and display logout
+    const auth = localStorage.getItem('user');
+
+    //to fix bog by redenring
+    const navigate = useNavigate();
+// function for logout 
+    const logout =()=> {
+        localStorage.clear();
+        navigate("/signup")
+    }
     return(
         <div>
             <ul className='nav-ul'>
                 <li><Link to="/">Food Category</Link></li>
                 <li><Link to="/add">Add Food</Link></li>
                 <li><Link to="/update">Update Food</Link></li>
-                <li><Link to="/logout">LogOut</Link></li>
                 <li><Link to="/profile">Profile</Link></li>
-                <li><Link to="/signup">Sign Up</Link></li>
+                <li>
+                    { auth ?<Link onClick={logout} to="/signup">LogOut</Link> :
+                    <Link to="/signup">Sign Up</Link>}</li>
+                    <li><Link to="/login">Login</Link></li>
             </ul>
         </div>
     )
