6583b0e00de48a8177e219c1009da9b89da9f280
Nabin788 <123879198+Nabin788@users.noreply.github.com>
2023-06-05 22:10:24 +0545

    Add files via upload

---
+++ WebApp/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static('views'));
+app.set('view engine', 'ejs');
+
+// Render index.ejs for the root URL
+app.get("/", (req, res) => {
+    res.render("index2.ejs");
+});
+
+app.listen(1001, () => {
+    console.log("Server is running on port 1000");
+});
+
+
+
+
+
+
+
