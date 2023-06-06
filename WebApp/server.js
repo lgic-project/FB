53015dff594ddd7f789abfae095094419e748e08
Nabin788 <123879198+Nabin788@users.noreply.github.com>
2023-06-07 01:41:57 +0545

    Update server.js

--- WebApp/server.js
+++ WebApp/server.js
@@ -6,11 +6,15 @@ app.set('view engine', 'ejs');
 
 // Render index.ejs for the root URL
 app.get("/", (req, res) => {
-    res.render("index2.ejs");
+    res.render("index.ejs");
+});
+
+app.get("/order", (req, res) => {
+    res.render("order.ejs");
 });
 
 app.listen(1001, () => {
-    console.log("Server is running on port 1000");
+    console.log("Server is running on port 1001");
 });
 
 
