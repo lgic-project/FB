d3ef31d74197116282bfe0aa1cb1240c34b12278
Nabin788 <123879198+Nabin788@users.noreply.github.com>
2023-06-14 15:16:03 +0545

    Update server.js

--- project/server.js
+++ project/server.js
@@ -1,24 +1,23 @@
-const mongoose = require("mongoose")
-require("./mconfig/mdb")
+const mongoose = require("mongoose") // Importing the mongoose library
+require("./mconfig/mdb") // Importing the MongoDB configuration
 
-require("dotenv").config();
+require("dotenv").config(); // Loading environment variables from .env file
 
+const express = require("express") // Importing the Express library
+const app = express() // Creating an instance of the Express application
 
-const express = require("express")
-const app = express()
+const port = 2023 // Setting the port number for the server
 
-const port = 2023
+const bcrypt = require("bcrypt") // Importing the bcrypt library for password hashing
 
-const bcrypt = require("bcrypt")
+app.use(express.json()); // Parsing JSON data in the request body
 
-app.use(express.json());
+app.use("users", require("./routes/userRoutes")) // Using userRoutes for handling routes starting with "/users"
 
-app.use("users", require("./routes/userRoutes"))
+app.listen(port,()=>{ // Starting the server and listening on the specified port
+    console.log("server lsiten from port", port); // Logging a message indicating the server is listening on the port
 
-app.listen(port,()=>{
-    console.log("server lsiten from port", port);
-
-    if (port == port) {
-        console.log("database is connected");
+    if (port == port) { // Checking if the port variable matches the specified port
+        console.log("database is connected"); // Logging a message indicating the database is connected
     }
 })
