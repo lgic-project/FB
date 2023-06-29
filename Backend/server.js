079752007df7a97415b880d19b40cfba39c7ea54
Nabin788 <123879198+Nabin788@users.noreply.github.com>
2023-06-30 02:46:41 +0545

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,8 @@ const registerRoutes = require("./routes/register");
 const loginRoutes = require("./routes/login");
 const logoutRoutes = require("./routes/logout");
 const { authenticateToken } = require("./middleware/auth");
+const foods = require("./routes/menu");
+const order = require("./routes/order");
 
 
 const cookieParser = require("cookie-parser");
@@ -25,6 +27,12 @@ app.use("/api/users", registerRoutes, loginRoutes, logoutRoutes);
 app.get("/home", authenticateToken, (req, res) => {
   res.send('Welcome to the home page!');
 });
+
+// menu routes
+app.use("/api/food", foods);
+
+// order routes
+app.use("/api/order", order);
 
 app.listen(PORT, () => {
   console.log("Server listening on port", PORT);
