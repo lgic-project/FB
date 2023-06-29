ff9dd2094d81b61c4fb830d63dd763fad89910cc
Nabin788 <123879198+Nabin788@users.noreply.github.com>
2023-06-30 01:18:37 +0545

    Update login.js

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -1,51 +1,9 @@
-const { User } = require("../models/user");
-
 const router = require("express").Router();
-const joi = require("joi");
-const bcrypt = require("bcrypt");
-
-router.post("/login", async (req, res) => {
-    try {
-        const { error } = validate(req.body);
-        if (error)
-            return res
-                .status(400)
-                .send({ message: error.details[0].message });
+const loginController = require("../controllers/login");
 
 
-        const user = await User.findOne({ email: req.body.email });
-        if (!user)
-            return res
-                .status(401)
-                .send({ message: "Invalid email or password" });
+router.post("/login", loginController);
 
-        const validPassword = await bcrypt.compare(
-            req.body.password, user.password
-        );
-        if (!validPassword)
-            return res
-                .status(401)
-                .send({ message: "Invalid email or password" })
 
-        const token = user.generateAuthToken();
-        res
-            .status(200)
-            .send({ data: token, message: "LogIn successfully" })
-
-    } catch (error) {
-        res
-            .status(500)
-            .send({ message: "Internal Server error" })
-
-    }
-})
-
-const validate = (data) => {
-    const schema = joi.object({
-        email: joi.string().email().required().label("Email"),
-        password: joi.string().required().label("Password")
-    });
-    return schema.validate(data);
-}
 
 module.exports = router;
