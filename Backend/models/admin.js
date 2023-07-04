2ca2dfb5a6e470132ad6239ad747db609f1fb0c1
Nabin788 <123879198+Nabin788@users.noreply.github.com>
2023-07-04 18:34:00 +0545

    Add files via upload

---
+++ Backend/models/admin.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+const adminSchema = new mongoose.Schema({
+  adminname: {
+    type: String,
+    required: [true, "Please insert your name"]
+  },
+  email: {
+    type: String,
+    required: [true, "Please insert your email address"]
+  },
+  password: {
+    type: String,
+    minlength: [8, "Password must be at least 8 characters"]
+  }
+});
+
+adminSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d"
+  });
+  return token;
+};
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+const validate = (data) => {
+  const schema = joi.object({
+    adminname: joi.string().required().label("adminname"),
+    email: joi.string().email().required().label("Email"),
+    password: passwordComplexity().required().label("Password")
+  });
+  return schema.validate(data);
+};
+
+module.exports = { Admin, validate};
