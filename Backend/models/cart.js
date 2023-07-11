d5f7b133714455c12cda3b8fde7e4677db58d445
Nabin  Poudel <123879198+Nabin788@users.noreply.github.com>
2023-07-11 23:46:53 +0545

    Add files via upload

---
+++ Backend/models/cart.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  items: [{
+    foodItemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'food',
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true
+    }
+  }]
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+module.exports = Cart;
