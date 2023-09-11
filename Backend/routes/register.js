15e31dc8c3481067254e2de59c9de68138c3dff7
Nabin  Poudel <123879198+Nabin788@users.noreply.github.com>
2023-09-11 21:30:27 +0545

    Update and rename register.js to user.js

--- Backend/routes/register.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const registerUser = require("../controllers/register");
-
-router.post("/register", registerUser);
-
-
-
-module.exports = router;
---
+++ Backend/routes/user.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const {registerUser, loginUser, logoutUser} = require("../controllers/user");
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+
+module.exports = router;
