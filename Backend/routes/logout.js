ee771c066a7bdcd4b994985cb375434d1acad200
Nabin  Poudel <123879198+Nabin788@users.noreply.github.com>
2023-09-11 21:29:57 +0545

    Delete Backend/routes/logout.js

--- Backend/routes/logout.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const logoutController = require("../controllers/logout");
-
-
-router.post("/logout", logoutController);
-
-
-module.exports = router;
