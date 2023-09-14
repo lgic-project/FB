a8ad93bdaee2946e34ff3024968b0ae8c12315cf
Nabin  Poudel <123879198+Nabin788@users.noreply.github.com>
2023-09-14 08:51:39 +0545

    Update menu.js

--- Backend/routes/menu.js
+++ Backend/routes/menu.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 const {
   addFood,
   getFoods,
+  getFoodById,
   updateFood,
   deleteFood
 } = require("../controllers/menu");
 
 router.post("/addFood", addFood);
 router.get("/foods", getFoods);
+router.get("/:id", getFoodByID);
 router.put("/:id", updateFood);
 router.delete("/:id", deleteFood);
 
