91782050a9fb47cdb8a4c408f7fd85d71a63a218
Nabin  Poudel <123879198+Nabin788@users.noreply.github.com>
2023-09-15 22:02:00 +0545

    Update menu.js

--- Backend/controllers/menu.js
+++ Backend/controllers/menu.js
@@ -2,57 +2,23 @@ const Food = require("../models/menu");
 const upload = require("../middleware/multer");
 
 
-// const addFood = async (req, res) => {
-//   try {
-//     // Using multer for multiple image uploads
-//     upload.array('images', 3)(req, res, async function (err) {
-//       if (err) {
-//         return res.status(400).json({ error: 'Sorry, failed to upload images', message: err.message });
-//       }
-
-//       // Create a new food document with image file IDs (GridFS object IDs)
-//       const imageIds = req.files.map(file => file.id);
-
-//       const newFood = await Food.create({
-//         name: req.body.name,
-//         description: req.body.description,
-//         price: req.body.price,
-//         availability: req.body.availability,
-//         imageIds: imageIds, // Save the GridFS object IDs in the database
-//       });
-
-//       // Check if the food item and images are saved
-//       if (newFood) {
-//         console.log("Food and images saved successfully:", newFood);
-//         res.status(200).json({
-//           success: true,
-//           newFood,
-//           message: "Food added successfully"
-//         });
-//       } else {
-//         console.error("Failed to save food and images.");
-//         res.status(500).json({ error: "Internal server error" });
-//       }
-//     });
-//   } catch (error) {
-//     console.error("Error:", error);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// };
+const path = require("path");
 
 const addFood = async (req, res) => {
   try {
-    // Using multer for multiple image uploads
+    // multer for multiple image uploads
     upload.array('images', 3)(req, res, async function (err) {
       if (err) {
         return res.status(400).json({ error: 'Sorry, failed to upload images', message: err.message });
       }
 
       // Create a new food document with image URLs
-      const imageUrls = req.files.map(file => file.path); 
+      // const imageUrls = req.files.map(file => file.path);
+      const imageUrls = req.files.map(file => path.basename(file.path));
 
       const newFood = await Food.create({
         name: req.body.name,
+        quantity: req.body.quantity,
         description: req.body.description,
         price: req.body.price,
 
