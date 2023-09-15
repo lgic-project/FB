22ea8b2f3fe50916b473737afc141808b1a0bc81
Nabin  Poudel <123879198+Nabin788@users.noreply.github.com>
2023-09-15 09:41:54 +0545

    Update admin.js

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
-const {registerAdmin, loginAdmin, logoutAdmin} = require('../controllers/admin');
+const adminAuth = require("../middleware/adminAuth");
+const {registerAdmin, loginAdmin, logoutAdmin, getAdminData} = require('../controllers/admin');
 
 router.post('/register', registerAdmin);
 router.post('/login', loginAdmin);
 router.post("/logout", logoutAdmin);
+
+// Only logIn admin data can get
+router.get("/data", adminAuth, getAdminData); 
 
 module.exports = router;
