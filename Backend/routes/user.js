63242a7de11f0da1a09debda6911bb655a6765fb
Nabin  Poudel <123879198+Nabin788@users.noreply.github.com>
2023-09-15 10:42:44 +0545

    Update user.js

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const {registerUser, loginUser, logoutUser} = require("../controllers/user");
+const {registerUser, loginUser, logoutUser, getFoodByUser} = require("../controllers/user");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.get("/food", getFoodByUser)
 
 module.exports = router;
